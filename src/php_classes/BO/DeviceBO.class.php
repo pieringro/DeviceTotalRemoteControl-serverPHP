521f01256bc5d1ece8a270669012abb0a8c238c1
Pierluigi Ingrosso <pier_ip@hotmail.it>
2017-05-22 12:47:37 +0000

    Updated DeviceBO.class.php

--- src/php_classes/BO/DeviceBO.class.php
+++ src/php_classes/BO/DeviceBO.class.php
@@ -6,16 +6,24 @@ class DeviceBO {
 
     public function __construct() {
         $this->dao = AbstractDAO::getIstanceDAO("device");
+        $this->userDao = AbstractDAO::getIstanceDAO("user");
     }
 
     private $dao;
+    private $userDao;
     
     
     public function newDevice($deviceTO){
         if(($deviceTO instanceof DeviceTO)){
             if(isset($deviceTO->emailUser) && isset($deviceTO->passUser)){
                 //devo fare la login qui prima di creare il device
-                return $this->dao->create($deviceTO);
+                $userTO = new UserTO();
+                $userTO->email = $deviceTO->emailUser;
+                $userTO->pass = $deviceTO->passUser;
+                $loginSuccessful = $this->userDao->read($userTO);
+                if($loginSuccessful){
+                    return $this->dao->create($deviceTO);
+                }
             }
         }
         return false;
