e2c14390ee22d497325ee8deba535cf621129732
Pierluigi Ingrosso <pier_ip@hotmail.it>
2017-05-26 12:06:05 +0000

    Added file loginUser.php

---
+++ src/loginUser.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once ("php_func/constants.php");
+require_once ("php_func/clientComunication.php");
+require_once ("php_classes/bean/User.class.php");
+require_once ("php_classes/BO/UserBO.class.php");
+
+
+if (isset($_POST['data'])) {
+    try {
+        $userTO = User::getUserTOFromJson($_POST['data']);
+        
+        //logica di criptazione della password
+        if(isset($userTO->email) && isset($userTO->pass)){
+            $hashed_password = crypt($userTO->pass);
+            $userTO->pass = $hashed_password;
+
+            $userBO = new UserBO();
+            $result = $userBO->loginUser($userTO);
+
+            if ($result) {
+                ok();
+            }
+            else{
+                error("Unable to perform login. ".$userBO->lastErrorMessage);
+            }
+        }
+        else{
+            error("Unable to perform login. Missing user data.");
+        }
+
+        
+    } catch (Exception $e) {
+        error($e->getMessage());
+    }
+} else {
+    error("No data passed.");
+}
+
+
+
+
+?>
