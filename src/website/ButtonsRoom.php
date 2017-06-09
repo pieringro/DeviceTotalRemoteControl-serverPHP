483a1c80d9381aaf8e7408c7b227ee99b981d4a8
Pierluigi Ingrosso <pier_ip@hotmail.it>
2017-06-09 11:56:46 +0000

    Updated ButtonsRoom.php

--- src/website/ButtonsRoom.php
+++ src/website/ButtonsRoom.php
@@ -2,24 +2,29 @@
 require_once('../php_func/constants.php');
 require_once('../php_func/simple_html_dom.php');
 require_once(ROOT_WEB . "/php_classes/bean/User.class.php");
+require_once(ROOT_WEB . "/php_classes/BO/UserBO.class.php");
 
 //controllo che sia loggato
 session_start();
 if(isset($_SESSION['user']) && isset($_SESSION['idDevicesList'])){
-    $to = unserialize(@$_SESSION['user']);
+    $userTo = unserialize(@$_SESSION['user']);
     //inserira in session anche una variabile che identifica i diversi token dei dispositivi di quest'utente
     $deviceToken = "Valore preso dalla sessione";
     
+    $userBo = new UserBO();
+    $devicesTOList = $userBo->getDevicesToFromUser($userTo);
+    $devicesIdsStringForHtml = "";
+    foreach($devicesTO as $devicesTOList){
+        $devicesIdsStringForHtml .= "<option>".$devicesTO->Id."</option>";
+    }
     
-    if(isset($to) && $to != false && $to instanceof UserTO){
+    if(isset($userTo) && $userTo != false && $userTo instanceof UserTO){
         //loggato, carica il file html
         
     }
     $htmlString = file_get_contents('ButtonsRoom.htm');
     $htmlString = str_replace("@dato@", "Dato sostituito con una stringa da codice php!!!", $htmlString);
-    $htmlString = str_replace("@deviceToken@", 
-            "<option>".$deviceToken."</option>", 
-            $htmlString);
+    $htmlString = str_replace("@deviceToken@", $devicesIdsStringForHtml, $htmlString);
 
     echo $htmlString;
 }
