613a0d8d71bcac621535409be6dc372773ef5c7e
pierprogramm <pierprogramm@gmail.com>
2017-09-23 16:14:53 +0200

    Rinomino file uploaded con contatore se esistente 1

--- src/php_func/receiveFileUploaded.php
+++ src/php_func/receiveFileUploaded.php
@@ -1,27 +1,61 @@
 <?php
 
-function doUploadNow($userFolder) {
-    $completePath = FILES_FOLDER."".FILE_SEPARATOR_CHAR."".$userFolder;
+define('FILENAME_NUMBER_SEPARATOR', "_");
 
-    if (!file_exists($completePath)) {
-        mkdir($completePath, 0777, true);
+function doUploadNow($userFolder) {
+    $completeRealPath = FILES_FOLDER . "" . FILE_SEPARATOR_CHAR . "" . $userFolder;
+    $completeWebPath = FILES_FOLDER_WEB . "" . FILE_SEPARATOR_CHAR . "" . $userFolder;
+    if (!file_exists($completeRealPath)) {
+        mkdir($completeRealPath, 0777, true);
     }
-    
+
     // verifico che il file sia stato caricato
     if (!is_uploaded_file($_FILES['file']['tmp_name']) ||
             $_FILES['file']['error'] > 0) {
-        throw new Exception("Error uploading file. error=".$_FILES['file']['error']);
+        throw new Exception("Error uploading file. error=" . $_FILES['file']['error']);
     }
 
-    $filePath = $completePath."".FILE_SEPARATOR_CHAR."".$_FILES['file']['name'];
-    
-    if (move_uploaded_file($_FILES['file']['tmp_name'], $filePath)) {
-        $filePathWeb = FILES_FOLDER_WEB."".FILE_SEPARATOR_CHAR."".$userFolder."".FILE_SEPARATOR_CHAR."".$_FILES['file']['name'];
-        return $filePathWeb;
+    $fileRealName = $_FILES['file']['name'];
+    $fileRealPath = $completeRealPath . "" . FILE_SEPARATOR_CHAR . "" . $fileRealName;
+
+    //controllo che il file a quel path non esista, se esiste devo cambiare il nome
+    $counter = 0;
+    while (file_exists($fileRealPath)) {
+        $fileRealPath = UpdateFilename($fileRealPath, $counter);
+        $counter++;
     }
-    else{
+    
+    //muovo il file nel percorso del server
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $fileRealPath)) {
+        $fileWebPath = $completeWebPath . "" . FILE_SEPARATOR_CHAR . "" . $fileRealName;
+        return $fileWebPath;
+    } else {
         throw new Exception("Unable to upload file.");
     }
+    
 }
 
+function UpdateFilename($filename, $counter = null) {
+    $filenameExtension = "";
+    
+    //rimuovo l'estensione nel filename se presente
+    $indexOfDot = strrpos($filename, '.');
+    if ($indexOfDot !== false) {
+        $filenameExtension = substr($filename, $indexOfDot);
+        $filename = substr($filename, 0, $indexOfDot);
+    }
+    
+    //rimuovo il contatore precedente se presente
+    $indexOfNumberSeparator = strrpos($filename, FILENAME_NUMBER_SEPARATOR);
+    if ($indexOfNumberSeparator !== false) {
+        $filename = substr($filename, 0, $indexOfNumberSeparator);
+    }
+    
+    if(isset($counter)){
+        $filenameExtension = FILENAME_NUMBER_SEPARATOR . $counter . $filenameExtension;
+    }
+    
+    $tmpFilename = $filename . "" . $filenameExtension;
+    return $tmpFilename;
+}
 
