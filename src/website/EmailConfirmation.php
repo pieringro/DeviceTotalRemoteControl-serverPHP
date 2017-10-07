f0dbe10e606b80ad025a2e89201b872e1ae4d6ee
pierprogramm <pierprogramm@gmail.com>
2017-10-07 09:18:21 +0200

    Funzione e pagina conferma email inizio 1

--- src/website/EmailConfirmation.php
+++ src/website/EmailConfirmation.php
@@ -1,14 +1,49 @@
 <?php
 require_once('../config/constants.php');
-
+require_once(ROOT_WEB . "/php_classes/BO/PendingEmailBO.class.php");
+require_once(ROOT_WEB.'/engine/resources/ResourcesManager.class.php');
 
 
 //ricevo in GET una stringa criptata
+$cryptedString = $_GET[EMAIL_KEY_NAME];
+
 //provo a decriptarla usando ogni uuid della tabella dtrc_pending_email_user_confirmation
-//come chiave e come valore l'email corrispondente
+//come chiave, e come valore l'email corrispondente
+if(isset($cryptedString)){
+    $pendingEmailBO = new PendingEmailBO();
+    $match = $pendingEmailBO->decrypt($cryptedString);
+    if($match){
+        $msg = ResourcesManager::getResource("user_activated");
+        header("Location: LoginPage.php?message=$msg");
+        die();
+    }
+}
 //trovata la corrispondenza, elimino il record di dtrc_pending_email_user_confirmation
 //e abilito l'utente su dtrc_users (flag inactive)
 
-//occorre creare una BO per le pending email
+?>
 
-$cryptString = $_GET[EMAIL_KEY_NAME];
+
+<html>
+    <head>
+        <title></title>
+    </head>
+    
+    <body>
+<?php
+        if($match) {
+?>
+        <p>
+            
+        </p>
+<?php
+        }
+?>
+    </body>
+    
+</html>
+
+
+
+
+
